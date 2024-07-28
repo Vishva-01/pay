fadf8930e5a88510aaf255e1492ee2f79c01de3f
Vishva-01 <vishva01mail@gmail.com>
2024-07-28 18:18:13 +0530

    change in migrations

--- razor/settings.py
+++ razor/settings.py
@@ -129,7 +129,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-from decouple import config # type: ignore
+
 
 # Load Razorpay API credentials from environment variables
 RAZORPAY_KEY_ID ="rzp_test_sYDXizFjDxb4Vw"
